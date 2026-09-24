ce1fc34bff9e06e185cfb31340fec2e65d746e3f
Amador Sierra González <amadorsierragonzalez@uma.es>
2026-09-24 13:59:31 +0200

    Subida inicial del proyecto de Unity

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    Animator am;
+    PlayerMovement pm;
+    SpriteRenderer sr;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        am = GetComponent<Animator>();
+        pm = GetComponent<PlayerMovement>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        am.SetFloat("Horizontal", pm.moveDirection.x);
+        am.SetFloat("Vertical", pm.moveDirection.y);
+        am.SetFloat("Speed", pm.moveDirection.magnitude);
+
+    }
+
+    void SpriteDirectionChecker()
+    {
+    
+    }
+}
